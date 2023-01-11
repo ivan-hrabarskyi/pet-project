c06cd52d79c346d597522c22b997210115f66f45
IvanHrabarskyi <ivan.hrabarskyi@peerspace.com>
2023-01-11 09:00:04 +0200

    Updated

--- design-patterns/src/main/java/com/peerspace/design_patterns/factory_method/GuestName.java
+++ design-patterns/src/main/java/com/peerspace/design_patterns/factory_method/GuestName.java
@@ -3,6 +3,6 @@ package com.peerspace.design_patterns.factory_method;
 public class GuestName implements SenderName {
     @Override
     public void getSenderName() {
-        System.out.println("Host name is present in Guest");
+        System.out.println("Host name is present in Guest Inbox Page");
     }
 }
