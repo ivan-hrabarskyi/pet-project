d211718c78b897951ca26b9f61c2155ca57699f4
IvanHrabarskyi <ivan.hrabarskyi@peerspace.com>
2023-01-11 09:33:10 +0200

    Update Factory method

--- design-patterns/src/main/java/com/peerspace/design_patterns/factory_method/GuestName.java
+++
@@ -1,8 +0,0 @@
-package com.peerspace.design_patterns.factory_method;
-
-public class GuestName implements SenderName {
-    @Override
-    public void getSenderName() {
-        System.out.println("Host name is present in Guest Inbox Page");
-    }
-}
